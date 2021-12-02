222df0da463db7c52760287cb50d093fcf945a2c
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2021-12-02 12:10:01 +0100

    .

--- codecontemplator/day2/part1.ps1
+++ codecontemplator/day2/part1.ps1
@@ -2,7 +2,6 @@ $rows = Get-Content .\input.txt
 $data = $rows | % { $dir, $amount = $_.split(' '); [PSCustomObject]@{ Dir = $dir; Amount =$amount } }
 $data | % {[int]$x = 0; [int]$y=0} {
     $d = [int]$_.Amount 
-    Write-Host $d
     switch ($_.Dir) {
         "forward" { $x += $d }
         "down" { $y += $d }
