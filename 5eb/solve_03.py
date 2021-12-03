af0732f6f643c651daaff2029a0bd8698c77b903
Sebastian Bengtsson <sebastian.bengtsson@collectorbank.se>
2021-12-03 23:09:52 +0100

    cleanup

--- 5eb/solve_03.py
+++ 5eb/solve_03.py
@@ -8,30 +8,38 @@ def get_input(fn):
         indata = fh.read().splitlines()
     return indata
 
+
 bint = partial(int, base=2)
+
 
 def bit_lines(lines):
     return [[int(char) for char in line] for line in lines]
 
+
 def bit_cols(lines):
-    return [[int(char) for char in col] for col in zip(*lines)]
+    return bit_lines(zip(*lines))
+
 
 def common_bits(cols):
     return [round(mean(col)) for col in cols]
 
+
 def join_bits(bit_arr):
     return bint(''.join(str(bit) for bit in bit_arr))
+
 
 def complement(num):
     return bint('1' * num.bit_length()) ^ num
 
+
 def part_1(lines):
     gamma = join_bits(common_bits(bit_cols(lines)))
     epsilon = complement(gamma)
-    return gamma, epsilon
+    return gamma, epsilon, gamma * epsilon
 
-assert part_1(get_input('input03_test')) == (22, 9)
-# print(part_1(get_input('input03')))
+
+assert part_1(get_input('input03_test')) == (22, 9, 198)
+print(part_1(get_input('input03')))
 
 
 def school_round(floating):
@@ -42,7 +50,9 @@ def school_round(floating):
 def find_line(all_lines, neg=False):
     filtered_lines = all_lines
     for i in range(len(all_lines[0])):
-        bit = school_round(mean(list(zip(*filtered_lines))[i]))
+        bit = school_round(mean(
+            list(zip(*filtered_lines))[i]
+        ))
         if neg:
             bit = int(not bit)
         filtered_lines = [line for line in filtered_lines if line[i] == bit]
@@ -56,8 +66,8 @@ def part_2(lines):
     diagnostics_report = bit_lines(lines)
     oxygen_rating = join_bits(find_line(bit_lines(lines)))
     scrubber_rating = join_bits(find_line(bit_lines(lines), neg=True))
-    return oxygen_rating * scrubber_rating
+    return oxygen_rating, scrubber_rating, oxygen_rating * scrubber_rating
 
 
-assert part_2(get_input('input03_test')) == 230
+assert part_2(get_input('input03_test')) == (23, 10, 230)
 print(part_2(get_input('input03')))
