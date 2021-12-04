dcd2c7166c698b408ea5b5c6b0b4f4f34ccd237e
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2021-12-04 20:41:52 +0100

    day 4 part 1

---
+++ codecontemplator/day4/main1.r
@@ -0,0 +1,44 @@
+#library(tidyverse)
+library(rlist)
+
+mark_value <- function(board, v) {
+    board$mask <- board$mask * (board$values != v)
+    board
+}
+
+is_done <- function(board) {
+    sum(c(apply(board$mask, 1, sum), apply(board$mask, 2, sum)) == 0) > 0
+}
+
+get_result <- function(board, v) {
+    sum(board$mask * board$values) * v
+}
+
+solve <- function(num_seq, boards) {
+    for (v in num_seq) {
+        for (i in 1:length(boards)) {
+            boards[[i]] <- mark_value(boards[[i]], v)
+            if (is_done(boards[[i]])) {
+                return(get_result(boards[[i]], v))
+            }
+        }
+    }
+    return(-1)
+}
+
+lines <- read_lines("input.txt")
+
+num_seq <- as.integer(unlist(strsplit(lines[1], ",")))
+
+board_rows <- lines[-1]
+board_chunks <- split(board_rows, ceiling(seq_along(board_rows) / 6))
+boards <- lapply(board_chunks, function(board_chunk) {
+    board_data <- paste(board_chunk, collapse = "\r\n")
+    values <- read_table(board_data, col_names = FALSE)
+    list(
+        values = values,
+        mask = values * 0 + 1   # 1 = unmarked, 0 = marked
+    )
+});
+
+solve(num_seq, boards)  # 39902
