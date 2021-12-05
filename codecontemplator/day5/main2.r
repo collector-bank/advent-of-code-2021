279fed96bfcfdb86fa7cead5bfe14002f4c4839e
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2021-12-05 15:54:47 +0100

    simplified code

--- codecontemplator/day5/main2.r
+++ codecontemplator/day5/main2.r
@@ -9,19 +9,13 @@ ys <- ys - min(ys) + 1
 m <- matrix(0, max(ys), max(xs))
 
 for (i in 1:n) {
-    xsi <- xs[i, ]
-    ysi <- ys[i, ]
-    x1 <- xsi[[1]]
-    x2 <- xsi[[2]]
-    y1 <- ysi[[1]]
-    y2 <- ysi[[2]]
+    x1 <- xs[i, 1]
+    x2 <- xs[i, 2]
+    y1 <- ys[i, 1]
+    y2 <- ys[i, 2]
 
     cs <- cbind(c(y1:y2), c(x1:x2))
-    for (j in 1:nrow(cs)) {
-        y <- cs[j, 1]
-        x <- cs[j, 2]
-        m[y, x] <- m[y, x] + 1
-    }
+    m[cs] <- m[cs] + 1
 }
 
 sum(m > 1) # 16793
