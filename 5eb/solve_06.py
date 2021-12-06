ac1b52c9726780b629148231478bf0a0e43d3126
Sebastian Bengtsson <sebastian.bengtsson@collectorbank.se>
2021-12-06 13:07:47 +0100

    solved part 2

--- 5eb/solve_06.py
+++ 5eb/solve_06.py
@@ -1,4 +1,4 @@
-from collections import Counter
+from collections import Counter, deque
 
 
 def read_input(fn):
@@ -7,36 +7,43 @@ def read_input(fn):
     return [int(n) for n in indata.strip().split(',')]
 
 
-def new_fish(old_fish):
-    new = []
-    for fish in old_fish:
-        if fish == 0:
-            new.append(8)
-    return new
-
-
 def log(fish, day):
-    print(f'After {day: >4} days:', repr(fish).strip('[]'))
+    print(f'After {day: >4} days:', repr(fish))
 
 
 def part1(filename, days):
     fishies = read_input(filename)
     log(fishies, 0)
     for day in range(days):
-        fishies = [fishie - 1 for fishie in fishies]
-        fishies.extend(Counter(fishies)[0] * [8])
-        fishies = [fishie or 6 for fishie in fishies]
+        fishies = [fish - 1 for fish in fishies]
+        fishies.extend(Counter(fishies)[-1] * [8])
+        fishies = [fish * (fish >= 0) + (fish < 0) * 6 for fish in fishies]
+        print(fishies, day + 1)
         log(fishies, day + 1)
     return len(fishies)
 
 
+def part2(filename, days):
+    fishies = read_input(filename)
+    first_count = Counter(fishies)
+    by_age = deque([first_count[age] for age in range(9)])
+    log(by_age, 0)
+    for day in range(days):
+        by_age.rotate(-1)
+        by_age[6] += by_age[8]
+        log(by_age, day + 1)
+    return sum(by_age)
+
+
 def main():
-    print(part1('input06_test', 18))
+    # print(part1('input06_test', 18))
     # assert part1('input06_test', 18) == 26
     # assert part1('input06_test', 80) == 5934
-    # print(part1('input06'))
-    # assert part2('input06_test') == 12
-    # print(part2('input06'))
+    # print(part1('input06', 80))
+    assert part2('input06_test', 18) == 26
+    assert part2('input06_test', 80) == 5934
+    assert part2('input06_test', 256) == 26984457539
+    print(part2('input06', 256))
 
 
 if __name__ == '__main__':
