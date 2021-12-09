554f19dc1bc846fa8edc0453901b2bb2f2052626
Sebastian Bengtsson <sebastian.bengtsson@collectorbank.se>
2021-12-09 02:44:10 +0100

    Solved day 8

--- 5eb/solve_08.py
+++ 5eb/solve_08.py
@@ -34,6 +34,7 @@ digit_segments = {
     8: 'abcdefg',
     9: 'abcdfg',
 }
+segment_digits = {frozenset(segs): d for (d, segs) in digit_segments.items()}
 
 dark_segments = {d: ''.join(set('abcdefg') - set(segs)) for (d, segs) in digit_segments.items()}
 {
@@ -77,44 +78,58 @@ def part1(filename):
 
 
 def decode_patterns(unique_signal_patterns):
-    translator = {
-        'abcdefg': 8,
+    translator = {}
+    reverse = {}
+    distinct_lengths = {
+        2: 1,
+        3: 7,
+        4: 4,
+        7: 8,
+    }
+    possible_by_length = {
+        6: {0, 6, 9},
+        5: {2, 3, 5},
     }
 
-    # connections = {}
-    # usps = [''.join(sorted(pattern)) for pattern in unique_signal_patterns]
-    # [
-    #     'abcdefg',  'bcdef',  'acdfg',  'abcdf',  'abd',
-    #     'abcdef',  'bcdefg',  'abef',  'abcdeg',  'ab',
-    # ]
-    # print(usps)
-    # dark = [''.join(set('abcdefg') - set(segs)) for segs in unique_signal_patterns]
-    # print('dark:', dark)
-    # patterns = [set(p) for p in usps]
-    # cf = next(p for p in patterns if len(p) == 2)
-    # acf  = next(p for p in patterns if len(p) == 3)
-    # connections['a'] = acf - cf
-    # bcdf = next(p for p in patterns if len(p) == 4)
-    # bd = bcdf - cf
+    for pattern in unique_signal_patterns:
+        pattern_length = len(pattern)
+        if pattern_length in distinct_lengths:
+            translator[pattern] = distinct_lengths[pattern_length]
+            reverse[distinct_lengths[pattern_length]] = pattern
 
-    facit = {
-        frozenset('abcdefg'): 8,
-        frozenset('cdfbe'): 5,
-        frozenset('gcdfa'): 2,
-        frozenset('fbcad'): 3,
-        frozenset('dab'): 7,
-        frozenset('cefabd'): 9,
-        frozenset('cdfgeb'): 6,
-        frozenset('eafb'): 4,
-        frozenset('cagedb'): 0,
-        frozenset('ab'): 1,
-    }
-    return facit
+    cf = set(reverse[1])
+    acf  = set(reverse[7])
+    a = acf - cf
+    bcdf = set(reverse[4])
+    bd = bcdf - cf
+
+    for pattern in unique_signal_patterns:
+        if pattern in translator:
+            continue
+        possible_digits = possible_by_length[len(pattern)]
+        pat = set(pattern)
+        if len(pattern) == 6:  # {0, 6, 9}
+            if (bcdf | a).issubset(pat):
+                translator[pattern] = 9
+            elif cf.issubset(pat):
+                translator[pattern] = 0
+            elif bd.issubset(pat):
+                translator[pattern] = 6
+        elif len(pattern) == 5:  # {2, 3, 5}
+            if cf.issubset(pat):
+                translator[pattern] = 3
+            elif bd.issubset(pat):
+                translator[pattern] = 5
+            else:
+                translator[pattern] = 2
+    return translator
 
 
 def translate_output(unique_signal_patterns, output_value):
     translator = decode_patterns(unique_signal_patterns)
-    return int(''.join(str(translator[frozenset(segments)]) for segments in output_value))
+    set_translator = {frozenset(segs): d for (segs, d) in translator.items()}
+    set_output = [frozenset(segs) for segs in output_value]
+    return int(''.join(str(set_translator[segments]) for segments in set_output))
 
 
 def part2(filename):
@@ -126,8 +141,10 @@ def part2(filename):
 
 
 def main():
-    # print(part1('input08'))
+    print(part1('input08'))
     print(part2('input08_test1'))
+    print(part2('input08_test2'))
+    print(part2('input08'))
 
 
 if __name__ == '__main__':
--- 5eb/test_solve_08.py
+++ 5eb/test_solve_08.py
@@ -7,6 +7,7 @@ from solve_08 import (
     part1,
     translate_output,
     part2,
+    decode_patterns,
 )
 
 
@@ -31,7 +32,41 @@ def test_part1():
     assert part1('input08_test2') == 26
 
 
-class _TestPart2:
+class TestPart2:
+
+    def test_decode_patterns_normal_segments(self):
+        pattern = ['abcefg', 'cf', 'acdeg', 'acdfg', 'bcdf', 'abdfg', 'abdefg', 'acf', 'abcdefg', 'abcdfg']
+        expected = {
+            'abcefg': 0,
+            'cf': 1,
+            'acdeg': 2,
+            'acdfg': 3,
+            'bcdf': 4,
+            'abdfg': 5,
+            'abdefg': 6,
+            'acf': 7,
+            'abcdefg': 8,
+            'abcdfg': 9,
+        }
+        result = decode_patterns(pattern)
+        assert result == expected
+
+    def test_decode_patterns_example1(self):
+        pattern = ['acedgfb', 'cdfbe', 'gcdfa', 'fbcad', 'dab', 'cefabd', 'cdfgeb', 'eafb', 'cagedb', 'ab']
+        expected = {
+            'acedgfb': 8,
+            'cdfbe': 5,
+            'gcdfa': 2,
+            'fbcad': 3,
+            'dab': 7,
+            'cefabd': 9,
+            'cdfgeb': 6,
+            'eafb': 4,
+            'cagedb': 0,
+            'ab': 1,
+        }
+        result = decode_patterns(pattern)
+        assert result == expected
 
     def test_translate_output(self):
         unique_signal_patterns, output_value = [
