ed119c8705ec506c6a63bf89ac2df18f84929df6
Sebastian Bengtsson <sebastian.bengtsson@collectorbank.se>
2021-12-09 17:50:11 +0100

    break out select_basin

--- 5eb/solve_09.py
+++ 5eb/solve_09.py
@@ -28,13 +28,15 @@ def neighbour_indexes(row, col, shape):
     return indexes
 
 
+nditer = functools.partial(numpy.nditer, flags=['multi_index'])
+
+
 def find_low_points(height_map):
     low_points = numpy.zeros(height_map.shape, dtype=int)
     for level in range(10):
-        iterator = numpy.nditer(height_map, flags=['multi_index'])
-        for point in iterator:
+        for point in (it := nditer(height_map)):
             if point == level:
-                row, col = iterator.multi_index
+                row, col = it.multi_index
                 neighbours = []
                 for nb_index in neighbour_indexes(row, col, height_map.shape):
                     neighbours.append(height_map[nb_index])
@@ -54,27 +56,30 @@ def part1(filename):
     return total_risk_level(height_map)
 
 
+def select_basin(basin, start_coord, map_shape):
+    basin = basin.copy()  # Need to edit in place
+    indicator = 2
+    stack = {start_coord}
+    while stack:
+        position = stack.pop()
+        if basin[position] == 1:
+            basin[position] = indicator
+            stack |= set(neighbour_indexes(*position, map_shape))
+    return (basin == indicator).astype(int)
+
+
 def find_basins(height_map):
     map_shape = height_map.shape
     basin_map = (height_map < 9).astype(int)
     low_points = find_low_points(height_map)
 
     basins = []
-    iterator = numpy.nditer(low_points, flags=['multi_index'])
-    for low_point in iterator:
+    for low_point in (it := nditer(low_points)):
         if not low_point:
             continue
-        basin = basin_map.copy()
-        indicator = 2
-        here = iterator.multi_index
-
-        stack = {here}
-        while stack:
-            position = stack.pop()
-            if basin[position] == 1:
-                basin[position] = indicator
-                stack |= set(neighbour_indexes(*position, map_shape))
-        basins.append((basin == indicator).astype(int))
+        low_point_coord = it.multi_index
+        basin = select_basin(basin_map, low_point_coord, map_shape)
+        basins.append(basin)
     return basins
 
 
