2863dea71cb2aa8e588214cec6211641e4299fce
Sebastian Bengtsson <sebastian.bengtsson@collectorbank.se>
2021-12-09 03:02:26 +0100

    Remove unused code.

--- 5eb/solve_08.py
+++ 5eb/solve_08.py
@@ -8,20 +8,6 @@ def read_input(filename):
     return indata
 
 
-digit_segments_table = {
-    0: 'abc efg',
-    1: '  c  f ',
-    2: 'a cde g',
-    3: 'a cd fg',
-    4: ' bcd f ',
-    5: 'ab d fg',
-    6: 'ab defg',
-    7: 'a c  f ',
-    8: 'abcdefg',
-    9: 'abcd fg',
-}
-
-
 digit_segments = {
     0: 'abcefg',
     1: 'cf',
@@ -34,26 +20,22 @@ digit_segments = {
     8: 'abcdefg',
     9: 'abcdfg',
 }
-segment_digits = {frozenset(segs): d for (d, segs) in digit_segments.items()}
-
-dark_segments = {d: ''.join(set('abcdefg') - set(segs)) for (d, segs) in digit_segments.items()}
-{
-    0: 'd',
-    1: 'adgbe',
-    2: 'bf',
-    3: 'be',
-    4: 'eag',
-    5: 'ce',
-    6: 'c',
-    7: 'bedg',
-    8: '',
-    9: 'e',
-}
 
 lenseg_digits = defaultdict(set)
 for (d, segments) in digit_segments.items():
     lenseg_digits[len(segments)].add(d)
 lenseg_digits = dict(lenseg_digits)
+
+distinct_lengths = {
+    2: 1,
+    3: 7,
+    4: 4,
+    7: 8,
+}
+possible_by_length = {
+    6: {0, 6, 9},
+    5: {2, 3, 5},
+}
 
 
 def parse_line(line):
@@ -80,26 +62,15 @@ def part1(filename):
 def decode_patterns(unique_signal_patterns):
     translator = {}
     reverse = {}
-    distinct_lengths = {
-        2: 1,
-        3: 7,
-        4: 4,
-        7: 8,
-    }
-    possible_by_length = {
-        6: {0, 6, 9},
-        5: {2, 3, 5},
-    }
 
     for pattern in unique_signal_patterns:
         pattern_length = len(pattern)
-        if pattern_length in distinct_lengths:
-            translator[pattern] = distinct_lengths[pattern_length]
-            reverse[distinct_lengths[pattern_length]] = pattern
+        digit = distinct_lengths.get(pattern_length)
+        if digit:
+            translator[pattern] = digit
+            reverse[digit] = pattern
 
     cf = set(reverse[1])
-    acf  = set(reverse[7])
-    a = acf - cf
     bcdf = set(reverse[4])
     bd = bcdf - cf
 
@@ -108,28 +79,29 @@ def decode_patterns(unique_signal_patterns):
             continue
         possible_digits = possible_by_length[len(pattern)]
         pat = set(pattern)
-        if len(pattern) == 6:  # {0, 6, 9}
-            if (bcdf | a).issubset(pat):
-                translator[pattern] = 9
-            elif cf.issubset(pat):
-                translator[pattern] = 0
-            elif bd.issubset(pat):
-                translator[pattern] = 6
-        elif len(pattern) == 5:  # {2, 3, 5}
+        if len(pattern) == 5:  # {2, 3, 5}
             if cf.issubset(pat):
                 translator[pattern] = 3
             elif bd.issubset(pat):
                 translator[pattern] = 5
             else:
                 translator[pattern] = 2
+        if len(pattern) == 6:  # {0, 6, 9}
+            if (bcdf).issubset(pat):
+                translator[pattern] = 9
+            elif cf.issubset(pat):
+                translator[pattern] = 0
+            elif bd.issubset(pat):
+                translator[pattern] = 6
+
     return translator
 
 
 def translate_output(unique_signal_patterns, output_value):
     translator = decode_patterns(unique_signal_patterns)
-    set_translator = {frozenset(segs): d for (segs, d) in translator.items()}
-    set_output = [frozenset(segs) for segs in output_value]
-    return int(''.join(str(set_translator[segments]) for segments in set_output))
+    sorted_translator = {''.join(sorted(segments)): digit for (segments, digit) in translator.items()}
+    sorted_outputs = [''.join(sorted(segments)) for segments in output_value]
+    return int(''.join(str(sorted_translator[segments]) for segments in sorted_outputs))
 
 
 def part2(filename):
@@ -142,8 +114,6 @@ def part2(filename):
 
 def main():
     print(part1('input08'))
-    print(part2('input08_test1'))
-    print(part2('input08_test2'))
     print(part2('input08'))
 
 
