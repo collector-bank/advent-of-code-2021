077575b4923c523527f1bf45a637984bd516aab0
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2021-12-10 16:49:27 +0100

    cc - day 10 part 2

--- codecontemplator/day10/main.hs
+++ codecontemplator/day10/main.hs
@@ -1,18 +1,44 @@
+import Data.List (sort)
+import Text.Printf (printf)
 
-data Result = Success | Incomplete | Corrupted Char deriving Show
+data Result = Success | Incomplete [Char] | Corrupted Char deriving Show
 
-score (Corrupted c) = 
-    case c of
-        ')' -> 3
-        ']' -> 57
-        '}' -> 1197
-        '>' -> 25137
-        _ -> error "invalid char"        
-score _ = 0
+solvePart1 :: [Result] -> Int
+solvePart1 = sum . map scoreCorrupted
+    where
+        scoreCorrupted :: Result -> Int 
+        scoreCorrupted (Corrupted c) = 
+            case c of
+                ')' -> 3
+                ']' -> 57
+                '}' -> 1197
+                '>' -> 25137
+                _ -> error "invalid char"        
+        scoreCorrupted _ = 0
+
+solvePart2 :: [Result] -> Int
+solvePart2 = head . middle . sort . filter (/=0) . map scoreIncomplete
+    where
+        middle :: [a] -> [a]
+        middle l@(_:_:_:_) = middle $ tail $ init l
+        middle l           = l
+
+        scoreIncomplete :: Result -> Int
+        scoreIncomplete (Incomplete stack) = foldr ((\e s -> 5 * s + e) . scoreChar) 0 (reverse stack)
+            where
+                scoreChar :: Char -> Int
+                scoreChar c =
+                    case c of
+                        '(' -> 1
+                        '[' -> 2
+                        '{' -> 3
+                        '<' -> 4
+                        _ -> error "invalid char"
+        scoreIncomplete _ = 0
 
 parse :: [Char] -> [Char] -> Result
 parse [] [] = Success
-parse _ []  = Incomplete
+parse stack []  = Incomplete stack
 parse stack (x:xs) = 
     if x `elem` ['(', '{', '<', '['] then
         parse (x:stack) xs
@@ -27,4 +53,6 @@ parse stack (x:xs) =
         
 main = do
     input <- lines <$> readFile "input.txt"
-    print $ sum $ map (score . parse []) input
+    let seq = map (parse []) input
+    printf "part 1 = %d\n" $ solvePart1 seq --  469755
+    printf "part 2 = %d\n" $ solvePart2 seq -- 2762335572
