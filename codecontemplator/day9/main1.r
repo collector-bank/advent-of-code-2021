a9781b5f0af3408091791c897fca55d1e8c42786
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2021-12-10 17:11:41 +0100

    cleanup

--- codecontemplator/day9/main1.r
+++ codecontemplator/day9/main1.r
@@ -1,20 +1,11 @@
-print("hello world")
 d <- read.table("input.txt", header = FALSE)
-d
 
 nr <- dim(d)[1]
 nc <- dim(d)[2]
 
 sright <- cbind(rep(10,nr), d[,1:nc-1])
 sleft <- cbind(d[,2:nc], rep(10,nr))
-
 sup <- rbind(d[2:nr,], rep(10,nc))
 sdown <- rbind(rep(10,nc), d[1:nr-1,])
-sright
-sleft
-sup
-sdown
 
-print("-------------")
-
-sum(d[d < sup & d < sdown & d < sright & d < sleft]+1)
+sum(d[d < sup & d < sdown & d < sright & d < sleft] + 1)
