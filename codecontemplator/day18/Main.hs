cce8f74bc65f931e45bb0f3a82db227f06d69208
Daniel Brännström <daniel.brannstrom@collectorbank.se>
2021-12-18 23:25:30 +0100

    cleanup

--- codecontemplator/day18/Main.hs
+++ codecontemplator/day18/Main.hs
@@ -5,23 +5,20 @@ instance Show Number where
     show (Pair a b) = "[" ++ show a ++ "," ++ show b ++ "]"
 
 parse :: String -> Number
-parse = snd . go 
+parse = snd . go
     where
         go :: String -> (String,Number)
-        go (x:xs) = 
+        go (x:xs) =
             if x == '[' then
-                let (xs',l) = go xs        
+                let (xs',l) = go xs
                     (xs'',r) = go (tail xs')
                 in (tail xs'',Pair l r)
             else
                 (xs, Regular (read [x] :: Int))
         go _ = error "failed"
 
-
--- test = parse "[[[[7,1],[0,0]],[6,[8,2]]],[8,[3,8]]]"
-
 add :: Number -> Number -> Number
-add a b = reduce $ Pair a b 
+add a b = reduce $ Pair a b
 
 reduce :: Number -> Number
 reduce n =
@@ -32,28 +29,25 @@ reduce n =
                 (n', True) -> reduce n'
                 (_, False) -> n
 
-data InstructionLeft = NoneL | AddLeft Int
-data InstructionRight = NoneR | AddRight Int 
-
 explode :: Number -> (Number, Bool)
 explode n = case go 0 n of (n', _ , _, found) -> (n', found)
-    where 
-        go :: Int -> Number -> (Number, InstructionLeft, InstructionRight, Bool)
-        go _ n@(Regular _) = (n, NoneL, NoneR, False)
-        go depth n@(Pair (Regular a) (Regular b)) = 
+    where
+        go :: Int -> Number -> (Number, Maybe Int, Maybe Int, Bool)
+        go _ n@(Regular _) = (n, Nothing , Nothing , False)
+        go depth n@(Pair (Regular a) (Regular b)) =
             if depth >= 4 then
-                (Regular 0, AddLeft a, AddRight b, True)
+                (Regular 0, Just a, Just b, True)
             else
-                (n, NoneL, NoneR, False)
+                (n, Nothing , Nothing , False)
         go depth (Pair a b) =
             case go (depth+1) a of
-                (a', inl, AddRight x, True  ) -> (Pair a' (incLeft b x), inl, NoneR, True)
-                (a', inl, NoneR     , True  ) -> (Pair a' b            , inl, NoneR, True)
-                (_ , _  ,          _, False ) -> 
+                (a', inl, Just x , True  ) -> (Pair a' (incLeft b x), inl, Nothing , True)
+                (a', inl, Nothing, True  ) -> (Pair a' b            , inl, Nothing , True)
+                (_ , _  ,       _, False ) ->
                     case go (depth+1) b of
-                        (b', AddLeft x, inr, True)  -> (Pair (incRight a x) b', NoneL, inr, True   )
-                        (b', NoneL    , inr, True)  -> (Pair a b'             , NoneL, inr, True   )
-                        (_ , _        , _  , False) -> (Pair a b              , NoneL, NoneR, False)
+                        (b', Just x , inr, True)  -> (Pair (incRight a x) b', Nothing , inr, True      )
+                        (b', Nothing, inr, True)  -> (Pair a b'             , Nothing , inr, True      )
+                        (_ , _      , _  , False) -> (Pair a b              , Nothing , Nothing , False)
 
         incLeft :: Number -> Int -> Number
         incLeft (Regular x) i = Regular (x + i)
@@ -64,60 +58,36 @@ explode n = case go 0 n of (n', _ , _, found) -> (n', found)
         incRight (Pair a b) i = Pair a (incRight b i)
 
 
--- test2 = fst $ explode $ parse "[[[[[9,8],1],2],3],4]"
--- a2 = show test2 == "[[[[0,9],2],3],4]"
-
--- test3 = fst $ explode $ parse "[7,[6,[5,[4,[3,2]]]]]"
--- a3 = show test3 == "[7,[6,[5,[7,0]]]]"
-
--- test4 = fst $ explode $ parse "[[6,[5,[4,[3,2]]]],1]"
--- test5 = fst $ explode $ parse "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"
--- test6 = fst $ explode $ parse "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
-
 split :: Number -> (Number, Bool)
-split n = go n
-    where
-        go n@(Regular x) = 
-            if x >= 10 then 
-                (Pair (Regular (div x 2)) (Regular (div x 2 + rem x 2)), True)
-            else
-                (n, False)
-        go (Pair a b) = 
-            case go a of
-                (a', True) -> (Pair a' b, True)
-                (_ , False) -> 
-                    case go b of 
-                        (b', True) -> (Pair a b', True)
-                        _ -> (Pair a b, False)
-
--- test7 = 
---     let l = parse "[[[[4,3],4],4],[7,[[8,4],9]]]"
---         r = parse "[1,1]"
---     in
---         add l r  -- [[[[0,7],4],[[7,8],[6,0]]],[8,1]]
+split n@(Regular x) =
+    if x >= 10 then
+        (Pair (Regular (div x 2)) (Regular (div x 2 + rem x 2)), True)
+    else
+        (n, False)
+split (Pair a b) =
+    case split a of
+        (a', True) -> (Pair a' b, True)
+        (_ , False) ->
+            case split b of
+                (b', True) -> (Pair a b', True)
+                _ -> (Pair a b, False)
 
 sumN :: [Number] -> Number
-sumN = foldl1 add          
-
--- test8 = sumN $ map parse ["[1,1]", "[2,2]", "[3,3]", "[4,4]"]  -- [[[[1,1],[2,2]],[3,3]],[4,4]]
--- test9 = sumN $ map parse ["[1,1]", "[2,2]", "[3,3]", "[4,4]", "[5,5]"]
--- test10 = sumN $ map parse ["[1,1]", "[2,2]", "[3,3]", "[4,4]", "[5,5]", "[6,6]"]
+sumN = foldl1 add
 
 magnitude :: Number -> Int
 magnitude (Regular x) = x
-magnitude (Pair a b) =
-    let ma = magnitude a
-        mb = magnitude b
-    in
-        3 * ma + 2 * mb
+magnitude (Pair a b) = 3 * magnitude a + 2 * magnitude b
 
 maxMag :: [Number] -> Int
-maxMag ns = maximum  [ max (magnitude (sumN [x,y])) (magnitude (sumN [y,x])) | x <- ns, y <- ns ]
+maxMag ns = 
+    let pairs = concat [ [[x,y],[y,x]] | x <- ns, y <- ns ]
+    in maximum . map (magnitude . sumN) $ pairs
+
 main = do
     numbers <- map parse . lines <$> readFile "input.txt"
-    print "# part 1"
-    print $ "sum : " ++ show (sumN numbers)
-    print $ "magnitude : " ++ show (magnitude $ sumN numbers)
-    print "# part 2"
-    print $ "max magnitude : " ++ show (maxMag numbers)
+    print "# part 1, magnitude of sum"
+    print $ magnitude . sumN $ numbers
+    print "# part 2, max magnitude"
+    print $ maxMag numbers
 
